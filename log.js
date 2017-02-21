96c8dfc84edb9d64279869fe5c19c1b3aa10671f
Michael Arnold <michael.arnold@lifeway.com>
2017-02-21 14:58:46 -0600

    Update to logic

--- log.js
+++ log.js
@@ -2,22 +2,28 @@
 
 module.exports.log = (event, context, callback) => {
   const data = JSON.parse(event.body);
-  if (typeof data.note !== 'string') {
+  if (typeof data.title !== 'string') {
     console.error('Validation Failed');
-    callback(new Error('Couldn\'t add the note.'));
+    callback(new Error('Couldn\'t log page visit.'));
     return;
   }
 
+  const date = new Date();
+  const isoTime = date.toISOString();
+
   const params = {
-    Message: data.note,
+    site: data.site,
+    referrer: data.referrer,
+    browser: data.browser,
+    resolution: data.resolution,
+    page: data.page,
+    title: data.title,
+    timestamp: isoTime
   };
 
   const response = {
     statusCode: 200,
-    body: JSON.stringify({
-      message: data.note,
-      input: event,
-    }),
+    body: JSON.stringify(params),
   };
 
   callback(null, response);
