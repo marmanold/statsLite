074650d8dfa0fa3b2d8bad7a6b4a1e3caaabda36
Michael Arnold <michael.arnold@lifeway.com>
2017-03-16 16:48:36 -0500

    Update to stats function

--- stats.js
+++ stats.js
@@ -6,6 +6,8 @@ const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 module.exports.stats = (event, context, callback) => {
 
+  const data = JSON.parse(event.body);
+
   const params = {
     TableName: process.env.DYNAMODB_TABLE,
     IndexName: process.env.DYNAMODB_SITE_INDEX,
@@ -23,19 +25,18 @@ module.exports.stats = (event, context, callback) => {
     // handle potential errors
     if (error) {
       console.error(error);
-      callback(new Error('Couldn\'t fetch the todo item.'));
+      callback(new Error('Couldn\'t query to database.'));
       return;
     }
 
     // create a response
     const response = {
       statusCode: 200,
-      body: JSON.stringify(result),
+      body: JSON.stringify(result.Items),
     };
     callback(null, response);
   });
 
 };
 
-//curl https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats/pico
-//event.pathParameters.id
+//curl -X POST https://bz2gq8s1sb.execute-api.us-east-1.amazonaws.com/dev/stats --data '{"site":"www.marmanold.com"}'
